8f63b0776d1794f1e4d58f76811b39a7a8dbf6df
* <arithmetic@protonmail.com>
2018-08-23 22:39:36 -0500

    Adding Color.h file for digital website

---
+++ Colors/Color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+class Color {
+string white;
+string green;
+string yellow;
+string black;
+public:
+DisplayColor();
+{
+//Coloring
+//white
+white = "\0xbeba17f4";
+//green
+green = "\0xbeac67f4";
+//yellow
+yellow = "\0xbe93c7f4";
+//black
+black = "\0xbe9797f4";
+};
+#endif
