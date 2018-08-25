13dfba2e0afc961a17c44fe0e0a776b7ee7e63ec
* <arithmetic@protonmail.com>
2018-08-25 00:28:42 -0500

    Adding Algorithm header file

---
+++ Algorithm/OWHAlgorithm.h
@@ -0,0 +1,21 @@
+#ifndef OWHALGORITHM_H
+#define OWHALGORITHM_H
+
+class OWHalgorithm {
+string Hash;
+int Salt;
+int port;
+int server;
+OWHHash(server, port,  Hash)
+{
+//Server Function
+  *owh -> (&Hash);
+Hash = "\0xbebe97e8";
+Salt = "\0xbe9c77e8";
+
+if (server != port)
+{
+ server -> (&Hash);
+}
+  };
+
